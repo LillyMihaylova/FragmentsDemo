14fa6254dc6a90187193b039eb5f8e7e5e1ca37a
Lilly Mihaylova <liliya.mihaylova@inveitix.com>
2018-06-06 17:16:36 +0300

    initial commit

---
+++ app/src/main/java/com/example/demo/fragmentsdemoapplication/fargments/ThirdFragment.java
@@ -0,0 +1,20 @@
+package com.example.demo.fragmentsdemoapplication.fargments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.demo.fragmentsdemoapplication.R;
+
+public class ThirdFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_fragment_third, container, false);
+    }
+}
